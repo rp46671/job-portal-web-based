f5484936c20087e1f9ec98b17857879e722689fe
rp46671 <ravip8527@gmail.com>
2020-06-09 00:27:50 +0530

    ravipandey

---
+++ application/views/dashbord/FeedBack.php
@@ -0,0 +1,13 @@
+          <div class="main-content-container container-fluid px-4">
+            <!-- Page Header -->
+            <div class="page-header row no-gutters py-4">
+              <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
+                <h3 class="page-title">FeedBack</h3>
+              </div>
+            </div>
+            <div class="row">
+
+
+               </div>
+           </div>
+         
---
+++ application/views/dashbord/underconstruct.php
@@ -0,0 +1,13 @@
+<div class="main-content-container container-fluid px-4">
+  <div class="page-header row no-gutters py-4">
+    <div class="col-12 col-sm-4 text-center text-sm-left mb-0">
+       <h3 class="page-title">Underconstucter</h3>
+    </div>
+    </div>
+    <div class="row">
+       <div class="col-md-12">
+            <img src="<?php echo base_url('assets/images/unde.gif') ;?>" style="width:130%">
+        </div>
+    </div>
+</div>
+         
